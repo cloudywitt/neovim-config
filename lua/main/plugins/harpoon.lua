a98e4853488447f5e71a7a4db2ad4f724521a8de
Ângelo Garcia <fhzkngj@gmail.com>
2023-12-10 20:38:14 -0400

    Add harpoon plugin

---
+++ lua/main/plugins/harpoon.lua
@@ -0,0 +1,8 @@
+return {
+    {"nvim-lua/plenary.nvim"}, -- don't forget to add this one if you don't have it yet!
+    {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        requires = { {"nvim-lua/plenary.nvim"} }
+    },
+}
